030a6bb6130066008f2a2d028ff53c450af359b8
YutongDu <yutong.du@aalto.fi>
2024-10-11 09:10:27 +0300

    add 8-1

--- 5-2-1/todo-api/deps.js
+++ 5-2-1/todo-api/deps.js
@@ -1,1 +1,4 @@
 // TODO
+import postgres from "https://deno.land/x/postgresjs@v3.4.4/mod.js";
+export { postgres };
+
---
+++ 8-1-1/app.svelte
@@ -0,0 +1,12 @@
+<script>
+    let list = ["Never", "gonna", "give", "you", "up"];
+    let index = 0;
+
+    const updateIndex = () => {
+        index++;
+        index = index === 5 ? 0 : index;
+    };
+</script>
+
+<h1>{list[index]}</h1>
+<button on:click={updateIndex}>Next!</button>
